5ac9d3a6f906666d4be923eaad3b1543ec3d35f7
leejc <leejc831@naver.com>
2024-04-10 20:06:43 +0900

    https://www.develop-blog.pe.kr 변경

--- src/components/AppPortfolio1.vue
+++ src/components/AppPortfolio1.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>내 블로그</h2>
-    <h5>주소 : https://www.develop-blog.shop</h5>
+    <h5>주소 : https://www.develop-blog.pe.kr</h5>
     <div v-if="loading" class="loading">로딩 중...</div>
     <iframe v-else :src="externalWebsiteUrl" width="100%" height="1000"></iframe>
   </div>
@@ -12,7 +12,7 @@ export default {
   data() {
     return {
       loading: true, 
-      externalWebsiteUrl: 'https://www.develop-blog.shop' 
+      externalWebsiteUrl: 'https://www.develop-blog.pe.kr' 
     };
   },
   mounted() {
